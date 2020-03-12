e2156f9203f2fbee174eeb3fafedddfa4fd29840
Jennifer Wallace <jennikate@gmail.com>
2020-03-12 13:31:15 +0000

    Remove Auth.js getUser function as not needed

--- src/lib/Auth.js
+++ src/lib/Auth.js
@@ -11,13 +11,6 @@ class Auth {
     localStorage.removeItem('token');
   }
 
-  static getUserId() {
-    const token = this.getToken();
-    if (!token) return false;
-    const parts = token.split('.');
-    return JSON.parse(atob(parts[1])).sub;
-  }
-
   static isAuthorized() {
     return this.getToken();
   }
