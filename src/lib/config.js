33506e16218d174d450c0f0554ba673cdc9e0ebc
Jennifer Wallace <jennikate@gmail.com>
2020-03-13 15:58:09 +0000

    Resolve linter problem

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,4 +1,4 @@
 
-// eslint-disable-next-line import/prefer-default-export
 // disabling linter as we cannot export default here (it doesn't work!)
+// eslint-disable-next-line import/prefer-default-export
 export const apiPath = 'http://localhost:5000/v1';
