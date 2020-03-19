50c62f29466ba022578dedc9e03debf9164bdc0e
Jennifer Wallace <jennikate@gmail.com>
2020-03-19 16:24:21 +0000

    Add responsible person form and data to checkDetails page

--- src/components/Forms/FormVoyageResponsiblePerson.jsx
+++ src/components/Forms/FormVoyageResponsiblePerson.jsx
@@ -4,185 +4,97 @@ const FormVoyageResponsiblePerson = ({ handleSubmit, handleChange, data }) => {
   return (
     <section>
       <h1 className="govuk-heading-xl">Responsible person</h1>
-      <p className="govuk-body-l">Provide the departure details of the voyage</p>
+      <p className="govuk-body-l">Provide the details of the person responsible for this voyage. By default this is the Skipper, but could be the vessel owner if this is a charter.</p>
 
-      <div id="departureDate" className="govuk-form-group">
-        <fieldset className="govuk-fieldset" role="group" aria-describedby="departureDate-hint">
-          <legend className="govuk-fieldset__legend govuk-fieldset__legend--m">
-            <label className="govuk-label govuk-label--m" htmlFor="departureDate">
-              Departure date
-            </label>
-          </legend>
-          <span className="govuk-hint">
-            For example, 20 2 2020
-          </span>
-          <div className="govuk-date-input" >
-            <div className="govuk-date-input__item">
-              <div className="govuk-form-group">
-                <label className="govuk-label govuk-date-input__label" htmlFor="departureDateDay">
-                  Day
-                </label>
-                <input
-                  className="govuk-input govuk-date-input__input govuk-input--width-2"
-                  name="departureDateDay"
-                  type="text"
-                  pattern="[0-9]*"
-                  inputMode="numeric"
-                  maxLength="2"
-                  value={data.departureDateDay || ''}
-                  onChange={(e) => handleChange(e)}
-                />
-              </div>
-            </div>
-            <div className="govuk-date-input__item">
-              <div className="govuk-form-group">
-              <label className="govuk-label govuk-date-input__label" htmlFor="departureDateMonth">
-                  Month
-                </label>
-                <input
-                  className="govuk-input govuk-date-input__input govuk-input--width-2"
-                  name="departureDateMonth"
-                  type="text"
-                  pattern="[0-9]*"
-                  inputMode="numeric"
-                  maxLength="2"
-                  value={data.departureDateMonth || ''}
-                  onChange={(e) => handleChange(e)}
-                />
-              </div>
-            </div>
-            <div className="govuk-date-input__item">
-              <div className="govuk-form-group">
-                <label className="govuk-label govuk-date-input__label" htmlFor="departureDateYear">
-                  Year
-                </label>
-                <input
-                  className="govuk-input govuk-date-input__input govuk-input--width-4"
-                  name="departureDateYear"
-                  type="text"
-                  pattern="[0-9]*"
-                  inputMode="numeric"
-                  maxLength="4"
-                  value={data.departureDateYear || ''}
-                  onChange={(e) => handleChange(e)}
-                />
-              </div>
-            </div>
-          </div>
-        </fieldset>
-      </div>
-
-      <div id="departureTime" className="govuk-form-group">
-        <fieldset className="govuk-fieldset" role="group" aria-describedby="departureTime-hint">
-          <legend className="govuk-fieldset__legend govuk-fieldset__legend--m">
-            <label className="govuk-label govuk-label--m" htmlFor="departureTime">
-              Departure time (UTC)
-            </label>
-          </legend>
-          <span className="govuk-hint">
-            For example, 17 30
-          </span>
-          <div className="govuk-date-input" >
-            <div className="govuk-date-input__item">
-              <div className="govuk-form-group">
-                <label className="govuk-label govuk-date-input__label" htmlFor="departureTimeHour">
-                  Hour
-                </label>
-                <input
-                  className="govuk-input govuk-date-input__input govuk-input--width-2"
-                  name="departureTimeHour"
-                  type="text"
-                  pattern="[0-9]*"
-                  inputMode="numeric"
-                  maxLength="2"
-                  value={data.departureTimeHour || ''}
-                  onChange={(e) => handleChange(e)}
-                />
-              </div>
-            </div>
-            <div className="govuk-date-input__item">
-              <div className="govuk-form-group">
-              <label className="govuk-label govuk-date-input__label" htmlFor="departureTimeMinute">
-                  Minute
-                </label>
-                <input
-                  className="govuk-input govuk-date-input__input govuk-input--width-2"
-                  name="departureTimeMinute"
-                  type="text"
-                  pattern="[0-9]*"
-                  inputMode="numeric"
-                  maxLength="2"
-                  value={data.departureTimeMinute || ''}
-                  onChange={(e) => handleChange(e)}
-                />
-              </div>
-            </div>
-          </div>
-        </fieldset>
-      </div>
-
-      <div id="departurePort" className="govuk-form-group">
-        <label className="govuk-label govuk-label--m" htmlFor="departurePort">
-          Departure port
+      <div id="responsibleGivenName" className="govuk-form-group">
+        <label className="govuk-label govuk-label--m" htmlFor="responsibleGivenName">
+          Given name
         </label>
-          <span className="govuk-hint">
-            The name or UNLOCODE of the port, for example Monaco or MC MON. Enter ‘ZZZZ’ if not known
-          </span>
         <input
           className="govuk-input"
-          name="departurePort"
+          name="responsibleGivenName"
           type="text"
-          value={data.departurePort || ''}
+          value={data.responsibleGivenName || ''}
           onChange={(e) => handleChange(e)}
         />
       </div>
 
-      <div id="departureCoordinates" className="govuk-form-group">
-        <fieldset className="govuk-fieldset" role="group" aria-describedby="departureCoordinates-hint">
-          <legend className="govuk-fieldset__legend govuk-fieldset__legend--m">
-            <label className="govuk-label govuk-label--m" htmlFor="departureCoordinates">
-              Departure coordinates
-            </label>
-            <span className="govuk-hint">
-              If port is ‘ZZZZ’, provide decimal coordinates to 6 places
-            </span>
-          </legend>
-          <div className="govuk-form-group">
-            <label className="govuk-label govuk-date-input__label" htmlFor="departureCoordinatesLat">
-              Latitude
-            </label>
-            <span className="govuk-hint">
-              For example 51.507493
-            </span>
-            <input
-              className="govuk-input govuk-date-input__input"
-              name="departureCoordinatesLat"
-              type="text"
-              pattern="[0-9]*"
-              inputMode="numeric"
-              value={data.departureCoordinatesLat || ''}
-              onChange={(e) => handleChange(e)}
-            />
-          </div>
-          <div className="govuk-form-group">
-            <label className="govuk-label govuk-date-input__label" htmlFor="departureCoordinatesLong">
-              Longitude
-            </label>
-            <span className="govuk-hint">
-              For example 51.507493
-            </span>
-            <input
-              className="govuk-input govuk-date-input__input"
-              name="departureCoordinatesLong"
-              type="text"
-              pattern="[0-9]*"
-              inputMode="numeric"
-              value={data.departureCoordinatesLong || ''}
-              onChange={(e) => handleChange(e)}
-            />
-          </div>
-        </fieldset>
+      <div id="responsibleSurname" className="govuk-form-group">
+        <label className="govuk-label govuk-label--m" htmlFor="responsibleSurname">
+          Surname
+        </label>
+        <input
+          className="govuk-input"
+          name="responsibleSurname"
+          type="text"
+          value={data.responsibleSurname || ''}
+          onChange={(e) => handleChange(e)}
+        />
       </div>
+      <div id="responsibleContactNo" className="govuk-form-group">
+        <label className="govuk-label govuk-label--m" htmlFor="responsibleContactNo">
+          Contact telephone number
+        </label>
+          <span className="govuk-hint">
+          Include international dialling code, for example +44
+          </span>
+        <input
+          className="govuk-input"
+          name="responsibleContactNo"
+          type="text"
+          value={data.responsibleContactNo || ''}
+          onChange={(e) => handleChange(e)}
+        />
+      </div>
+
+      <fieldset className="govuk-fieldset">
+        <legend className="govuk-fieldset__legend govuk-fieldset__legend--m">Address</legend>
+        <div id="responsibleAddressLine1" className="govuk-form-group">
+          <label className="govuk-label" htmlFor="responsibleAddressLine1">
+            House number / building and street
+            <span className="govuk-visually-hidden">line 1 of 2</span>
+          </label>
+          <input
+            className="govuk-input"
+            name="responsibleAddressLine1"
+            type="text"
+            value={data.responsibleAddressLine1 || ''}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div id="responsibleAddressLine2" className="govuk-form-group">
+          <label className="govuk-label" htmlFor="responsibleAddressLine2">
+            <span className="govuk-visually-hidden">Building and street line 2 of 2</span>
+          </label>
+          <input
+            className="govuk-input"
+            name="responsibleAddressLine2"
+            type="text"
+            value={data.responsibleAddressLine2 || ''}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div id="responsibleTown" className="govuk-form-group">
+          <label className="govuk-label" htmlFor="responsibleTown">Town or city</label>
+          <input
+            className="govuk-input govuk-!-width-two-thirds"
+            name="responsibleTown"
+            type="text"
+            value={data.responsibleTown || ''}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div id="responsiblePostcode" className="govuk-form-group">
+          <label className="govuk-label" htmlFor="responsiblePostcode">Postcode or ZIP</label>
+          <input
+            className="govuk-input govuk-input--width-10"
+            name="responsiblePostcode"
+            type="text"
+            value={data.responsiblePostcode || ''}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+      </fieldset>
 
       <button
         className="govuk-button"
