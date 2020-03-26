c9216a6991e56e549bd5923268fb9b64caa31f1e
Jennifer Wallace <jennikate@gmail.com>
2020-03-26 11:45:23 +0000

    Setup EditPerson page

---
+++ src/components/Forms/EditPerson.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+const EditPerson = (props) => {
+  console.log(props);
+
+  return (
+    <>
+      Yo
+    </>
+  );
+};
+
+export default withRouter(EditPerson);
