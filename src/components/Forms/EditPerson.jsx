ed3772b3d1bf267f6e50438569a7b4ca556aabec
Jennifer Wallace <jennikate@gmail.com>
2020-03-26 14:00:37 +0000

    Format and PATCH data

--- src/components/Forms/EditPerson.jsx
+++ src/components/Forms/EditPerson.jsx
@@ -5,7 +5,9 @@ import axios from 'axios';
 // App imports
 import Auth from 'Auth';
 import CreatePerson from 'CreatePerson';
+import { formatDate } from 'Utils/date';
 import { PEOPLE_URL } from 'Constants/ApiConstants';
+import { PEOPLE_PAGE_URL } from 'Constants/ClientConstants';
 
 
 const EditPerson = (props) => {
@@ -14,7 +16,7 @@ const EditPerson = (props) => {
   const [formData, setFormData] = useState();
   const [errors, setErrors] = useState({});
 
-  // reformat dates & peopleType into individual items for form
+  // reformat dates & peopleType into individual items for form field display
   const reformatFields = (data) => {
     const tempObj = data;
     const splitDoB = data.dateOfBirth.split('-');
@@ -28,6 +30,29 @@ const EditPerson = (props) => {
     tempObj.documentExpiryDateDay = splitDocumentExpiryDate[2];
     setPersonData(tempObj);
   };
+
+  // reformat dates for api
+  const reformatDate = () => {
+    if (!formData) {
+      return null;
+    }
+    const tempObj = formData;
+    // If a date field has changed, reformat date from personData and save back to personData
+    if (personData.documentExpiryDateYear || personData.documentExpiryDateMonth || personData.documentExpiryDateDay) {
+      tempObj.documentExpiryDate = formatDate(personData.documentExpiryDateYear, personData.documentExpiryDateMonth, personData.documentExpiryDateDay);
+    }
+    if (personData.dateOfBirthYear || personData.dateOfBirthMonth || personData.dateOfBirthDay) {
+      tempObj.dateOfBirth = formatDate(personData.dateOfBirthYear, personData.dateOfBirthMonth, personData.dateOfBirthDay);
+    }
+    delete tempObj.documentExpiryDateYear;
+    delete tempObj.documentExpiryDateMonth;
+    delete tempObj.documentExpiryDateDay;
+    delete tempObj.dateOfBirthYear;
+    delete tempObj.dateOfBirthMonth;
+    delete tempObj.dateOfBirthDay;
+    setFormData(tempObj);
+  };
+
 
   // Get data to prepopulate the form for this person
   const getPersonData = () => {
@@ -53,7 +78,7 @@ const EditPerson = (props) => {
 
   // Update form info to state
   const handleChange = (e) => {
-    // original data
+    // // original data
     setPersonData({ ...personData, [e.target.name]: e.target.value });
     // updated data
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -63,8 +88,37 @@ const EditPerson = (props) => {
   // Clear formData from localStorage
   const clearLocalStorage = () => {
     setPersonData({});
+    setFormData({});
     setErrors({ });
   };
+
+
+  // Handle Submit, including clearing localStorage
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // if date fields exist, format them
+    reformatDate();
+
+    axios.patch(`${PEOPLE_URL}/${personId}`, formData, {
+      headers: { Authorization: `Bearer ${Auth.retrieveToken()}` },
+    })
+      .then(() => {
+        clearLocalStorage();
+        history.push(PEOPLE_PAGE_URL);
+      })
+      .catch((err) => {
+        if (err.response) {
+          switch (err.response.status) {
+            case 400: console.log(err.data); break;
+            case 401: history.push(`/sign-in?source=${location}`); break;
+            case 422: history.push(`/sign-in?source=${location}`); break;
+            case 405: history.push(`/sign-in?source=${location}`); break;
+            default: history.push(`/sign-in?source=${location}`);
+          }
+        }
+      });
+  };
+
 
   // Get person data to pass to prepopulate the form
   useEffect(() => {
@@ -75,12 +129,11 @@ const EditPerson = (props) => {
   useEffect(() => {
     localStorage.setItem('data', JSON.stringify(personData));
   }, [personData]);
-
   useEffect(() => {
     localStorage.setItem('errors', JSON.stringify(errors));
   }, [errors]);
 
-  console.log(formData)
+  // console.log(formData);
 
   return (
     <div className="govuk-width-container ">
@@ -119,6 +172,7 @@ const EditPerson = (props) => {
 
               <CreatePerson
                 handleChange={handleChange}
+                handleSubmit={handleSubmit}
                 data={personData}
                 errors={errors}
               />
