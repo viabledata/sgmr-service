7136cc099598078929897b4e4babd181cc8c19ac
Jennifer Wallace <jennikate@gmail.com>
2020-04-08 10:50:04 +0100

    Update lint rules to handle test/spec files

--- src/__tests__/testSample2.test.js
+++ src/__tests__/testSample2.test.js
@@ -1,13 +1,11 @@
 /* eslint-disable react/jsx-filename-extension */ // Test files have react components and are .js so this rule doesn't apply
-// Test setup
-
 import React from 'react';
 import { shallow } from 'enzyme';
 
 // App imports
 import Main from '@components/Main';
 
-describe('First React component test with Enzyme', () => {
+describe('App loads', () => {
   it('renders without crashing', () => {
     shallow(<Main />);
   });
