8ad38084bb1057732b9f77a2302d31729d9262e3
Jennifer Wallace <jennikate@gmail.com>
2020-04-21 10:47:30 +0100

    Map vessel data to page

--- src/components/Vessel/VesselTable.jsx
+++ src/components/Vessel/VesselTable.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
 const VesselTable = (data) => {
-  // console.log(sourceForm)
-  console.log(data);
   return (
     <table className="table-clickable govuk-table">
       <thead className="govuk-table__head">
@@ -18,19 +16,23 @@ const VesselTable = (data) => {
         </tr>
       </thead>
       <tbody className="govuk-table__body">
-        <tr className="govuk-table__row">
-          {data.checkboxes === 'true' && (
-          <td className="govuk-table__cell multiple-choice--hod">
-            <div className="govuk-checkboxes__item">
-              <input type="checkbox" className="govuk-checkboxes__input jsCheckbox" id="radio-1" />
-              <label className="govuk-label govuk-checkboxes__label" htmlFor="radio-1">&nbsp;</label>
-            </div>
-          </td>
-          )}
-          <td className="govuk-table__cell">Baroness</td>
-          <td className="govuk-table__cell">Yacht</td>
-          <td className="govuk-table__cell">Lee-on-Solent</td>
-        </tr>
+        {data.vesselData.map((vessel) => {
+          return (
+            <tr className="govuk-table__row" key={vessel.id}>
+              {data.checkboxes === 'true' && (
+              <td className="govuk-table__cell multiple-choice--hod">
+                <div className="govuk-checkboxes__item">
+                  <input type="checkbox" className="govuk-checkboxes__input jsCheckbox" id={vessel.id} />
+                  <label className="govuk-label govuk-checkboxes__label" htmlFor={vessel.id}>&nbsp;</label>
+                </div>
+              </td>
+              )}
+              <td className="govuk-table__cell">{vessel.vesselName}</td>
+              <td className="govuk-table__cell">{vessel.vesselType}</td>
+              <td className="govuk-table__cell">{vessel.moorings}</td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
