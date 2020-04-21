94a05b7a052722835f0f6ec990bfc6c02b2f4cc7
Jennifer Wallace <jennikate@gmail.com>
2020-04-21 10:41:30 +0100

    Add call to get vessel data

--- src/components/Voyage/FormVessels.jsx
+++ src/components/Voyage/FormVessels.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 // App imports
 import { getData } from '@utils/apiHooks';
+import { VESSELS_URL } from '@constants/ApiConstants';
 import VesselTable from '@components/Vessel/VesselTable';
 
 const FormVessels = () => {
   const [vesselData, setVesselData] = useState();
   // Get vessel data
+  const storeVesselData = () => {
+    getData(VESSELS_URL)
+      .then((resp) => setVesselData(resp.items));
+  };
 
   // Display as checkboxes
   // Handle checkboxes
@@ -14,15 +19,22 @@ const FormVessels = () => {
   // Display vessel form
   // Save and continue saves to this voyage only (does not update vessel)
 
+  useEffect(() => {
+    storeVesselData();
+  }, []);
+
   return (
     <section>
       <h1 className="govuk-heading-xl">Vessel details</h1>
       <h2 className="govuk-heading-l">Saved vessels</h2>
       <p className="govuk-body-l">Add the details of a vessel you have saved previously to the report</p>
+      {vesselData && (
       <VesselTable
         vesselData={vesselData}
         sourceForm="voyage"
+        checkboxes="true"
       />
+      )}
     </section>
   );
 };
