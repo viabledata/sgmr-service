0491295b1a9cee2dac089eadcaf6bbcfe964114e
Jennifer Wallace <jennikate@gmail.com>
2020-04-27 10:59:44 +0100

    Update formatting

--- src/components/Voyage/FormVoyagePeople.jsx
+++ src/components/Voyage/FormVoyagePeople.jsx
@@ -24,20 +24,22 @@ const FormVoyagePeople = ({
     storePeopleData();
   }, []);
 
-
+  if (!peopleData) { return null; }
   return (
     <section>
       <h1 className="govuk-heading-xl">People on board</h1>
-      <h2 className="govuk-heading-l">Saved people</h2>
-      <p className="govuk-body-l">Add the details of people you have saved previously</p>
-      {peopleData && (
-        <PeopleTable
-          peopleData={peopleData}
-          sourceForm="voyage"
-          checkboxes="true"
-          link="false"
-          handleCheckboxes={handleCheckboxes}
-        />
+      {!peopleData.errors && (
+        <>
+          <h2 className="govuk-heading-l">Saved people</h2>
+          <p className="govuk-body-l">Add the details of people you have saved previously</p>
+          <PeopleTable
+            peopleData={peopleData}
+            sourceForm="voyage"
+            checkboxes="true"
+            link="false"
+            handleCheckboxes={handleCheckboxes}
+          />
+        </>
       )}
 
       <hr className="govuk-section-break govuk-section-break--m govuk-section-break--visible" />
@@ -49,20 +51,10 @@ const FormVoyagePeople = ({
       </p>
 
       <hr className="govuk-section-break govuk-section-break--m govuk-section-break--visible" />
-
-      {/* <hr className="govuk-section-break govuk-section-break--m govuk-section-break--visible">
-<h2 className="govuk-heading-l">New people</h2>
-<p>
-              <a href="create-report-add-new-person.html">Add a new person to the Reports</a>
-            </p>
-            <hr className="govuk-section-break govuk-section-break--m govuk-section-break--visible">
-
-            </hr>
-            <h2 className="govuk-heading-l">People currently on board</h2>
-            <p className="govuk-body-l">
-              People currently on the manifest of this report
-            </p> */}
-
+      <h2 className="govuk-heading-l">People currently on board</h2>
+      <p className="govuk-body-l">
+        People currently on the manifest of this report
+      </p>
       <PeopleManifest />
 
       <button
