cb9d779dbfaec5f0a01e66e4b477bc076cf1e377
Rafal Enden <git@enden.com>
2020-10-08 11:55:41 +0100

    Setup unit tests with react-testing-library

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,8 +5,10 @@ const Banner = () => {
     <div className="govuk-width-container app-width-container--wide">
       <div className="govuk-phase-banner">
         <p className="govuk-phase-banner__content">
-          <strong className="govuk-tag govuk-phase-banner__content__tag">alpha</strong>
-          <span className="govuk-phase-banner__text">
+          <strong className="govuk-tag govuk-pahase-banner__content__tag">
+            alpha
+          </strong>
+          <span className="govuk-phase-banner__text" data-testid="banner-text">
             This is a new service – your
             {' '}
             <a className="govuk-link" href="#">feedback</a>
--- sumTestSample.test.js
+++
@@ -1,7 +0,0 @@
-const sum = (a, b) => {
-  return a + b;
-};
-
-test('sum function', () => {
-  expect(sum(1, 3)).toBe(4);
-});
