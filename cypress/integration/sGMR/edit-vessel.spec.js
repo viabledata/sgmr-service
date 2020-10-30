abf50b86c9ad19e72a1d5ebb38c176c63768cff8
Boothiraj Palanisamy <bothees@gmail.com>
2020-10-30 14:08:58 +0000

    Review comments addressed

--- cypress/integration/sGMR/edit-vessel.spec.js
+++ cypress/integration/sGMR/edit-vessel.spec.js
@@ -37,12 +37,12 @@ describe('Edit existing vessel information', () => {
 
     cy.get('table').getTable().then((vesselData) => {
       expect(vesselData).to.not.be.empty;
-      expectedVessel.forEach((item) => expect(vesselData).to.deep.include(item));
+      expect(vesselData).to.deep.include(expectedVessel);
     });
   });
 
   it('Should be able to edit existing vessel information and NOT save', () => {
-    cy.get('a').contains(vessel.name).click();
+    cy.get('.govuk-table td a').contains(vessel.name).click();
     const expectedVessel = [
       {
         'Vessel name': vessel.name,
@@ -58,7 +58,7 @@ describe('Edit existing vessel information', () => {
 
     cy.get('table').getTable().then((vesselData) => {
       expect(vesselData).to.not.be.empty;
-      expectedVessel.forEach((item) => expect(vesselData).to.deep.include(item));
+      expect(vesselData).to.deep.include(expectedVessel);
     });
   });
 });
