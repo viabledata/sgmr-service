34e81731ac6169a7308af6c5c0d8f82bf148948a
Boothiraj Palanisamy <bothees@gmail.com>
2020-11-05 16:40:49 +0000

    Include Mailslurp client to setup inbox to receive password reset email

--- cypress/integration/sGMR/password-reset.spec.js
+++ cypress/integration/sGMR/password-reset.spec.js
@@ -1,48 +1,32 @@
 describe('User Request for Password reset', () => {
-  let email;
-  let inboxId;
-
   before(() => {
-    cy.createInbox().then((inbox) => {
-      // verify a new inbox was created
-      assert.isDefined(inbox);
-      email = inbox.emailAddress;
-      inboxId = inbox.id;
-      cy.fixture('user-registration.json').then((registrationData) => {
-        registrationData.email = email;
-        cy.registerUser(registrationData);
-        cy.visit('/sign-in');
-        cy.get('input[name="email"]').clear().type(email);
-        cy.get('input[name="password"]').clear().type('test1234');
-        cy.get('.govuk-button').click();
-        cy.server();
-        cy.route('POST', `${Cypress.env('api_server')}/login`).as('login');
-
-        cy.get('.govuk-button').click();
-        cy.url().should('include', '/verify?source=reports');
-
-        cy.wait('@login').should((xhr) => {
-          expect(xhr.status).to.eq(200);
-          let authCode = xhr.responseBody.twoFactorToken;
-          cy.get('input[name="twoFactorToken"]').clear().type(authCode);
-          cy.get('.govuk-button').click();
-        });
-        cy.navigation('Signout');
-        cy.url().should('include', '/sign-in');
-      });
-    });
+    cy.registerUser();
+    cy.login();
+    cy.navigation('Signout');
+    cy.url().should('include', '/sign-in');
   });
 
-  // eslint-disable-next-line jest/no-disabled-tests
-  it.skip('Should reset password succesfully', () => {
+  it('Should reset password succesfully', () => {
+    let apiServer = Cypress.env('api_server');
+    cy.visit('/sign-in');
     cy.contains('Problems signing in?').click();
-    cy.get('input[name="email"]').clear().type(email);
+    cy.get('input[name="email"]').clear().type('cb52ca14-0d64-4821-9970-4e81b47e13c1@mailslurp.com');
     cy.get('.govuk-button').click();
-    cy.log(inboxId);
-    cy.waitForLatestEmail(inboxId).then((mail) => {
-      // verify we received an email
+    cy.waitForLatestEmail('cb52ca14-0d64-4821-9970-4e81b47e13c1').then((mail) => {
       assert.isDefined(mail);
-      cy.log(mail.body);
+      const token = /token=([A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*)/.exec(mail.body)[1];
+      const email = /email=([A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,})/i.exec(mail.body)[1];
+      cy.log('token:', token, 'email', email);
+      cy.request({
+        url: `${apiServer}/activate-account`,
+        method: 'POST',
+        body: {
+          token: `${token}`,
+        },
+      }).then((activateResponse) => {
+        expect(activateResponse.status).to.eq(200);
+        expect(activateResponse.body.email).to.eq(email);
+      });
     });
   });
 });
