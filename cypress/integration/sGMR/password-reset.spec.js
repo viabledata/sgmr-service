10452a0cc998bddd3962067af67a9ee77e876684
Boothiraj Palanisamy <bothees@gmail.com>
2020-12-07 16:50:20 +0000

    Add tests for password reset functionality

--- cypress/integration/sGMR/password-reset.spec.js
+++ cypress/integration/sGMR/password-reset.spec.js
@@ -9,24 +9,55 @@ describe('User Request for Password reset', () => {
   it('Should reset password succesfully', () => {
     let apiServer = Cypress.env('api_server');
     cy.visit('/sign-in');
+    cy.server();
+    cy.route('POST', `${apiServer}/password-reset-confirmation`).as('confirmation');
     cy.contains('Problems signing in?').click();
-    cy.get('input[name="email"]').clear().type('cb52ca14-0d64-4821-9970-4e81b47e13c1@mailslurp.com');
+    cy.fixture('users.json').then((user) => {
+      cy.get('input[name="email"]').clear().type(user.email);
+    });
     cy.get('.govuk-button').click();
-    cy.waitForLatestEmail('cb52ca14-0d64-4821-9970-4e81b47e13c1').then((mail) => {
+    cy.get('.govuk-body-l').should('have.text', 'A link to reset your password was sent to the provided email address.');
+    cy.waitForLatestEmail('658bfbb0-47bc-4bb6-b256-412c1534b602').then((mail) => {
       assert.isDefined(mail);
       const token = /token=([A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*)/.exec(mail.body)[1];
-      const email = /email=([A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,})/i.exec(mail.body)[1];
-      cy.log('token:', token, 'email', email);
-      cy.request({
-        url: `${apiServer}/activate-account`,
-        method: 'POST',
-        body: {
-          token: `${token}`,
-        },
-      }).then((activateResponse) => {
-        expect(activateResponse.status).to.eq(200);
-        expect(activateResponse.body.email).to.eq(email);
-      });
+      cy.visit(`/new-password?token=${token}`);
     });
+    cy.fixture('users.json').then((user) => {
+      cy.get('input[name="password"]').clear().type(user.password);
+      cy.get('input[name="confirmPassword"]').clear().type(user.password);
+    });
+    cy.get('.govuk-button').click();
+    cy.wait('@confirmation').then((response) => {
+      expect(response.status).to.eq(204);
+    });
+  });
+
+  it('Should be logged-in with changed password', () => {
+    cy.visit('/sign-in');
+    cy.login();
+    cy.fixture('user-registration.json').then((userDetails) => {
+      cy.get('h3.govuk-heading-m').should('have.text', `Welcome back, ${userDetails.firstName}`);
+    });
+  });
+
+  it('Should not send reset email to unknown email address', () => {
+    let apiServer = Cypress.env('api_server');
+    cy.visit('/sign-in');
+    cy.server();
+    cy.route('POST', `${apiServer}/password-reset-confirmation`).as('confirmation');
+    cy.contains('Problems signing in?').click();
+    cy.get('input[name="email"]').clear().type('randomddd.mail@test.com');
+    cy.get('.govuk-button').click();
+    cy.get('.govuk-error-summary__list')
+      .should('be.visible')
+      .contains('Email address does not exist');
+  });
+
+  after(() => {
+    cy.deleteAllEmails();
+  });
+
+  afterEach(() => {
+    localStorage.removeItem('token');
   });
 });
