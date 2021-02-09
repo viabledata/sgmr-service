82f7364085e7e956332646e5bb52e6872fc9622f
Andrew Scrivener <andrew.scrivener@digital.homeoffice.gov.uk>
2021-02-09 12:10:56 +0000

    Added component for the 500 page
    Signed-off-by: Andrew Scrivener <andrew.scrivener@digital.homeoffice.gov.uk>

---
+++ src/components/500.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { govUrl } from 'config';
+
+const FiveHundred = () => {
+  return (
+    <div className="govuk-width-container ">
+      <main className="govuk-main-wrapper govuk-main-wrapper--auto-spacing" id="main-content" role="main">
+        <div className="govuk-grid-row">
+          <div className="govuk-grid-column-two-thirds">
+            <h1 className="govuk-heading-l">Sorry, the service is unavailable</h1>
+            <p className="govuk-body">This service is not available for the general public to use as we are still in private beta and testing the service.</p>
+            <p className="govuk-body">
+              We hope to launch the new service to the public later in 2021. When the service launches, you can give us feedback to help us improve it.
+            </p>  
+            <p className="govuk-body">
+              <a className="govuk-link" href="https://www.gov.uk/government/publications/notice-8-sailing-your-pleasure-craft-to-and-from-the-uk/">Check the sailing pleasure craft guidance</a> if you plan to sail to or from the UK.</p>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+export default FiveHundred;
