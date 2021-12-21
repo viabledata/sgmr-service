b89edb34514ec63243f22ddeb96a0bd752728953
JenniKate Wallace <jennikate@gmail.com>
2021-12-21 11:32:25 +0000

    Update tests to use describe/it format

--- src/components/Voyage/__tests__/VoyageFormValidation.test.js
+++ src/components/Voyage/__tests__/VoyageFormValidation.test.js
@@ -1,17 +1,19 @@
 import VoyageFormValidation from '@components/Voyage/VoyageFormValidation';
 import { FORM_STEPS } from '@constants/ClientConstants';
 
-test('Validates responsible person', async () => {
-  const expectedErrors = {
-    responsibleAddressLine1: 'You must enter the first line of your address',
-    responsibleAddressLine2: 'You must enter the second line of your address',
-    responsibleContactNo: 'You must enter a contact number',
-    responsibleCounty: 'You must enter a country name',
-    responsibleGivenName: 'You must enter a first name',
-    responsiblePostcode: 'You must enter a postcode',
-    responsibleSurname: 'You must enter a last name',
-    responsibleTown: 'You must enter a town or a city name',
-  };
-  const result = await VoyageFormValidation({}, FORM_STEPS.RESPONSIBLE_PERSON);
-  expect(result).toEqual(expectedErrors);
+describe('Voyage form validation', () => {
+  it('should validate responsible person', async () => {
+    const expectedErrors = {
+      responsibleAddressLine1: 'You must enter the first line of your address',
+      responsibleAddressLine2: 'You must enter the second line of your address',
+      responsibleContactNo: 'You must enter a contact number',
+      responsibleCounty: 'You must enter a country name',
+      responsibleGivenName: 'You must enter a first name',
+      responsiblePostcode: 'You must enter a postcode',
+      responsibleSurname: 'You must enter a last name',
+      responsibleTown: 'You must enter a town or a city name',
+    };
+    const result = await VoyageFormValidation({}, FORM_STEPS.RESPONSIBLE_PERSON);
+    expect(result).toEqual(expectedErrors);
+  });
 });
