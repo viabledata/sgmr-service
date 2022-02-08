9843d7fb1219be5c2789aa1eaba322da8ec45256
Adele Bampton <72464825+Adele-B@users.noreply.github.com>
2022-02-08 14:05:39 +0000

    Switch to using sessionStorage rather than local storage

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -57,7 +57,7 @@ const Nav = () => {
     })
       .then(() => {
         Auth.logout();
-        localStorage.clear();
+        sessionStorage.clear();
         history.push('/sign-in');
       })
       .catch(() => {
--- src/constants/ApiConstants.js
+++ src/constants/ApiConstants.js
@@ -3,7 +3,7 @@ import { apiUrl } from '../lib/config';
 export const LOGOUT_URL = `${apiUrl}/logout`;
 export const LOGIN_URL = `${apiUrl}/login`;
 export const USER_URL = `${apiUrl}/user`;
-export const USER_VOYAGE_REPORT_URL = `${apiUrl}/user/voyagereport`;
+export const USER_VOYAGE_REPORT_URL = `${apiUrl}/user/voyagereport?per_page=1000`;
 export const PASSWORD_RESET_LINK = `${apiUrl}/password-reset-link`;
 export const PASSWORD_RESET_CONFIRMATION = `${apiUrl}/password-reset-confirmation`;
 export const VOYAGE_REPORT_URL = `${apiUrl}/voyagereport`;
