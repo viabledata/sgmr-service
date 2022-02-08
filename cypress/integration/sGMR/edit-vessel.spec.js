f111167a0fae75e2be696e388a390dfac66a7180
JenniKate Wallace <jennikate@gmail.com>
2022-02-08 16:21:16 +0000

    Update table info in edit-vessel tests

--- cypress/integration/sGMR/edit-vessel.spec.js
+++ cypress/integration/sGMR/edit-vessel.spec.js
@@ -26,15 +26,15 @@ describe('Edit existing pleasure craft information', () => {
     cy.get('[name="vesselName"]').click().clear().type(vessel.name);
     cy.get('[name="vesselType"]').clear().type(vessel.type);
     cy.get('[name="moorings"]').clear().type(vessel.moorings);
-    cy.get('.govuk-button').click();
-    cy.get('.govuk-error-message').should('not.be.visible');
+    cy.get('.govuk-button').contains('Save').click();
+    cy.get('.govuk-error-message').should('not.exist');
 
     cy.get('table').getTable().then((vesselData) => {
       expect(vesselData).to.not.be.empty;
       expect(vesselData).to.deep.include({
-        'Pleasure craft name': vessel.name,
-        'Pleasure craft type': vessel.type,
-        'Usual moorings': vessel.moorings,
+        'Pleasure craft name': `Pleasure craft name${vessel.name}`,
+        'Pleasure craft type': `Pleasure craft type${vessel.type}`,
+        'Usual moorings': `Usual moorings${vessel.moorings}`,
       });
     });
   });
@@ -45,14 +45,14 @@ describe('Edit existing pleasure craft information', () => {
     cy.get('[name="vesselType"]').clear().type('Sailboat');
     cy.get('[name="moorings"]').clear().type('FR');
     cy.get('.govuk-link--no-visited-state').click();
-    cy.get('.govuk-error-message').should('not.be.visible');
+    cy.get('.govuk-error-message').should('not.exist');
 
     cy.get('table').getTable().then((vesselData) => {
       expect(vesselData).to.not.be.empty;
       expect(vesselData).to.deep.include({
-        'Pleasure craft name': vessel.name,
-        'Pleasure craft type': vessel.type,
-        'Usual moorings': vessel.moorings,
+        'Pleasure craft name': `Pleasure craft name${vessel.name}`,
+        'Pleasure craft type': `Pleasure craft type${vessel.type}`,
+        'Usual moorings': `Usual moorings${vessel.moorings}`,
       });
     });
   });
