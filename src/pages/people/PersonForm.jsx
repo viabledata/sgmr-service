8239e2becf19fc59f58b87d654d16c99c07119de
JenniKate Wallace <jennikate@gmail.com>
2022-02-18 11:32:01 +0000

    Add page for PersonForm

---
+++ src/pages/people/PersonForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PersonForm = () => {
+  console.log('person form');
+
+  return (
+    <>
+      <h1>Person</h1>
+      <p>person</p>
+    </>
+  );
+};
+
+export default PersonForm;
