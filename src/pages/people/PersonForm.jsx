28410efaa9f3193c501479472c0374aa1eb22f73
JenniKate Wallace <jennikate@gmail.com>
2022-02-18 12:12:38 +0000

    Switch title based on source

--- src/pages/people/PersonForm.jsx
+++ src/pages/people/PersonForm.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const PersonForm = () => {
-  console.log('person form');
+const PersonForm = ({ type, source }) => {
+  const [title, setTitle] = useState();
+
+  useEffect(() => {
+    if (type === 'edit') { source = 'edit'; }
+    switch (source) {
+      case 'onboarding':
+        setTitle('Add details of a person you frequently sail with');
+        break;
+      case 'voyage':
+        setTitle('Add details of the person you are sailing with');
+        break;
+      case 'edit':
+        setTitle('Update details of the person you sail with');
+        break;
+      default:
+        setTitle('Add details of the person you frequently sail with');
+    }
+  }, [source]);
+
+  document.title = type === 'edit' ? 'Edit person' : 'Save person';
 
   return (
-    <>
-      <h1>Person</h1>
-      <p>person</p>
-    </>
+    <div className="govuk-width-container ">
+      <main className="govuk-main-wrapper govuk-main-wrapper--auto-spacing" id="main-content" role="main">
+        <div className="govuk-width-container">
+          <h1 className="govuk-heading-l">{title}</h1>
+        </div>
+      </main>
+    </div>
   );
 };
 
