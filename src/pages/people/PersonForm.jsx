0ba0cf959482173a7ee787ae7fd6fce18bd5f16f
JenniKate Wallace <jennikate@gmail.com>
2022-02-18 13:56:31 +0000

    Add date of birth field

--- src/pages/people/PersonForm.jsx
+++ src/pages/people/PersonForm.jsx
@@ -43,11 +43,12 @@ const PersonForm = ({ type, source }) => {
               <h1 className="govuk-heading-l">{title}</h1>
 
               <div id="firstName" className="govuk-form-group">
-                <label className="govuk-label" htmlFor="firstName">
+                <label className="govuk-label" htmlFor="firstNameInput">
                   Given name(s)
                 </label>
                 <div id="firstName-hint" className="govuk-hint">The person&apos;s first and middle names</div>
                 <input
+                  id="firstNameInput"
                   className="govuk-input"
                   name="firstName"
                   type="text"
@@ -57,17 +58,84 @@ const PersonForm = ({ type, source }) => {
               </div>
 
               <div id="lastName" className="govuk-form-group">
-                <label className="govuk-label" htmlFor="lastName">
+                <label className="govuk-label" htmlFor="lastNameInput">
                   Surname
                 </label>
                 <div id="lastName-hint" className="govuk-hint">If the person has more than one surname, enter them all</div>
                 <input
+                  id="lastNameInput"
                   className="govuk-input"
                   name="lastName"
                   type="text"
                   value={formData?.lastName || ''}
                   onChange={handleChange}
                 />
+              </div>
+
+              <div id="dateOfBirth" className="govuk-form-group">
+                <label className="govuk-label" htmlFor="dateOfBirthDay">
+                  Date of birth
+                </label>
+                <div id="lastName-hint" className="govuk-hint">Enter this as shown on your passport, for example, 31 03 1980</div>
+                <div className="govuk-date-input">
+                  <div className="govuk-date-input__item">
+                    <div className="govuk-form-group">
+                      <label className="govuk-label govuk-date-input__label" htmlFor="dateOfBirthDay">
+                        Day
+                      </label>
+                      <input
+                        className="govuk-input govuk-date-input__input govuk-input--width-2"
+                        name="dateOfBirthDay"
+                        id="dateOfBirthDay"
+                        type="text"
+                        maxLength={2}
+                        autoComplete="bday-day"
+                        pattern="[0-9]*"
+                        inputMode="numeric"
+                        value={formData?.dateOfBirthDay || ''}
+                        onChange={(handleChange)}
+                      />
+                    </div>
+                  </div>
+                  <div className="govuk-date-input__item">
+                    <div className="govuk-form-group">
+                      <label className="govuk-label govuk-date-input__label" htmlFor="dateOfBirthMonth">
+                        Month
+                      </label>
+                      <input
+                        className="govuk-input govuk-date-input__input govuk-input--width-2"
+                        name="dateOfBirthMonth"
+                        id="dateOfBirthMonth"
+                        type="text"
+                        maxLength={2}
+                        autoComplete="bday-month"
+                        pattern="[0-9]*"
+                        inputMode="numeric"
+                        value={formData?.dateOfBirthMonth || ''}
+                        onChange={handleChange}
+                      />
+                    </div>
+                  </div>
+                  <div className="govuk-date-input__item">
+                    <div className="govuk-form-group">
+                      <label className="govuk-label" htmlFor="dateOfBirthYear">
+                        Year
+                      </label>
+                      <input
+                        className="govuk-input govuk-date-input__input govuk-input--width-4"
+                        name="dateOfBirthYear"
+                        id="dateOfBirthYear"
+                        type="text"
+                        maxLength={4}
+                        autoComplete="bday-year"
+                        pattern="[0-9]*"
+                        inputMode="numeric"
+                        value={formData?.dateOfBirthYear || ''}
+                        onChange={handleChange}
+                      />
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
