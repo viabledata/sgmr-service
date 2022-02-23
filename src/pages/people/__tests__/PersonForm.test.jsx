84d1f682274d5928f5d0142f63bf61ffde121e22
JenniKate Wallace <jennikate@gmail.com>
2022-02-23 14:19:55 +0000

    Refactor test

--- src/pages/people/__tests__/PersonForm.test.jsx
+++ src/pages/people/__tests__/PersonForm.test.jsx
@@ -5,43 +5,21 @@ import '@testing-library/jest-dom/extend-expect';
 import PersonForm from '../PersonForm';
 
 describe('Creating and editing people', () => {
-  it('should render page 1 of the people form when people page is loaded with a source of onboarding', () => {
-    render(
-      <MemoryRouter initialEntries={[{ pathname: '/page-1' }]}>
-        <PersonForm source="onboarding" />
-      </MemoryRouter>,
-    );
-    const title = screen.getByText('Add details of a person you frequently sail with');
-    expect(title.outerHTML).toEqual('<h1 class="govuk-heading-l">Add details of a person you frequently sail with</h1>');
-  });
+  it('should render different titles based on where the user comes from', () => {
+    render(<MemoryRouter initialEntries={[{ pathname: '/page-1' }]}><PersonForm source="onboarding" /></MemoryRouter>);
+    const titleOnboarding = screen.getByText('Add details of a person you frequently sail with');
+    expect(titleOnboarding.outerHTML).toEqual('<h1 class="govuk-heading-l">Add details of a person you frequently sail with</h1>');
 
-  it('should render page 1 of the people form when people page is loaded with a source of voyage', () => {
-    render(
-      <MemoryRouter initialEntries={[{ pathname: '/page-1' }]}>
-        <PersonForm source="voyage" />
-      </MemoryRouter>,
-    );
-    const title = screen.getByText('Add details of the person you are sailing with');
-    expect(title.outerHTML).toEqual('<h1 class="govuk-heading-l">Add details of the person you are sailing with</h1>');
-  });
+    render(<MemoryRouter initialEntries={[{ pathname: '/page-1' }]}><PersonForm source="voyage" /></MemoryRouter>);
+    const titleVoyage = screen.getByText('Add details of the person you are sailing with');
+    expect(titleVoyage.outerHTML).toEqual('<h1 class="govuk-heading-l">Add details of the person you are sailing with</h1>');
 
-  it('should render page 1 of the people form when people page is loaded with a source of edit', () => {
-    render(
-      <MemoryRouter initialEntries={[{ pathname: '/page-1' }]}>
-        <PersonForm source="edit" />
-      </MemoryRouter>,
-    );
-    const title = screen.getByText('Update details of the person you sail with');
-    expect(title.outerHTML).toEqual('<h1 class="govuk-heading-l">Update details of the person you sail with</h1>');
-  });
+    render(<MemoryRouter initialEntries={[{ pathname: '/page-1' }]}><PersonForm source="edit" /></MemoryRouter>);
+    const titleEdit = screen.getByText('Update details of the person you sail with');
+    expect(titleEdit.outerHTML).toEqual('<h1 class="govuk-heading-l">Update details of the person you sail with</h1>');
 
-  it('should render page 1 of the people form when people page is loaded with no source', () => {
-    render(
-      <MemoryRouter initialEntries={[{ pathname: '/page-1' }]}>
-        <PersonForm />
-      </MemoryRouter>,
-    );
-    const title = screen.getByText('Add details of the person you frequently sail with');
-    expect(title.outerHTML).toEqual('<h1 class="govuk-heading-l">Add details of the person you frequently sail with</h1>');
+    render(<MemoryRouter initialEntries={[{ pathname: '/page-1' }]}><PersonForm /></MemoryRouter>);
+    const titleNoSource = screen.getByText('Add details of the person you frequently sail with');
+    expect(titleNoSource.outerHTML).toEqual('<h1 class="govuk-heading-l">Add details of the person you frequently sail with</h1>');
   });
 });
