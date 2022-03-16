543ad23a7450b1b191943e7427fd2b6f50fd0fac
JenniKate Wallace <jennikate@gmail.com>
2022-03-16 11:53:25 +0000

    Remove attributes from test that no longer exist

--- src/components/Voyage/__tests__/FormArrival.test.jsx
+++ src/components/Voyage/__tests__/FormArrival.test.jsx
@@ -19,6 +19,5 @@ describe('FormArrival', () => {
     expect(screen.getAllByRole('textbox').length).toBe(6); // day, month, year, hour, minute
     expect(screen.getAllByRole('combobox').length).toBe(1); // port
     expect(screen.getByTestId('portContainer')).toHaveAttribute('id', 'portsCombobox');
-    expect(screen.getByTestId('portContainer')).toHaveAttribute('name', 'arrivalPort');
   });
 });
