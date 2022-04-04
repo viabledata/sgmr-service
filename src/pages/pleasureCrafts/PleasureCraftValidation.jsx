edbdf04ad642a36cb3c66d46a36ef1f86a9f60b8
JenniKate Wallace <jennikate@gmail.com>
2022-04-04 15:14:29 +0100

    Add form validation

---
+++ src/pages/pleasureCrafts/PleasureCraftValidation.jsx
@@ -0,0 +1,18 @@
+const pleasureCraftValidation = {
+  page1: [
+    {
+      inputField: 'pleasureCraftName',
+      errorDisplayId: 'pleasureCraftName',
+      type: 'required',
+      message: 'You must enter a pleasure craft name',
+    },
+    {
+      inputField: 'pleasureCraftType',
+      errorDisplayId: 'pleasureCraftType',
+      type: 'required',
+      message: 'You must enter a pleasure craft type',
+    },
+  ],
+};
+
+export default pleasureCraftValidation;
